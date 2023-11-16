9809020cb70eb7c4d6600d0d9beb45fcdb0ab709
Sai Krupa Telagamsetty <saikrupa.telagamsetty@factset.com>
2023-11-16 09:00:14 +0530

    test

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const heading =  React.createElement('h1',{id:'heading'},"Hello World From React
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+console.log(root)
 root.render(heading);
 
 
